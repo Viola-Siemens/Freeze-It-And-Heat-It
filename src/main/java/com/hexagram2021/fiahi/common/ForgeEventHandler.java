a433adcd29eff1fcfa297c05c132a8f6501d7dbb
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-08-25 20:38:19 +0800

    修改tick一个container的途径

--- src/main/java/com/hexagram2021/fiahi/common/ForgeEventHandler.java
+++ src/main/java/com/hexagram2021/fiahi/common/ForgeEventHandler.java
@@ -1,10 +1,20 @@
 package com.hexagram2021.fiahi.common;
 
 import com.hexagram2021.fiahi.common.handler.ItemStackFoodHandler;
+import com.hexagram2021.fiahi.common.util.FIAHILogger;
+import com.hexagram2021.fiahi.register.FIAHICapabilities;
+import dev.momostudios.coldsweat.api.util.Temperature;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Objects;
 
 import static com.hexagram2021.fiahi.FreezeItAndHeatIt.MODID;
 import static com.hexagram2021.fiahi.register.FIAHICapabilities.FOOD_CAPABILITY_ID;
@@ -16,5 +26,26 @@ public final class ForgeEventHandler {
 	@SubscribeEvent
 	public static void onAttackItemStackCapability(AttachCapabilitiesEvent<ItemStack> event) {
 		event.addCapability(FOOD_CAPABILITY_ID, new ItemStackFoodHandler(event.getObject()));
+	}
+
+	@SubscribeEvent
+	public static void onLevelTick(TickEvent.WorldTickEvent event) {
+		if(event.phase == TickEvent.Phase.END && event.world instanceof ServerLevel serverLevel) {
+			serverLevel.getChunkSource().chunkMap.getChunks().forEach(chunk -> {
+				LevelChunk levelChunk = chunk.getFullChunk();
+				if(levelChunk != null) {
+					levelChunk.getBlockEntities().forEach((blockPos, blockEntity) -> {
+						if (blockEntity.hasLevel() && blockEntity instanceof Container container) {
+							double temp = Temperature.getTemperatureAt(blockPos, Objects.requireNonNull(blockEntity.getLevel()));
+							FIAHILogger.debug("Temperature at (%d, %d, %d) is %f.".formatted(blockPos.getX(), blockPos.getY(), blockPos.getZ(), temp));
+							for (int i = 0; i < container.getContainerSize(); ++i) {
+								ItemStack food = container.getItem(i);
+								food.getCapability(FIAHICapabilities.FOOD_CAPABILITY).ifPresent(c -> c.foodTick(temp));
+							}
+						}
+					});
+				}
+			});
+		}
 	}
 }
--- src/main/java/com/hexagram2021/fiahi/mixin/ItemStackMixin.java
+++ src/main/java/com/hexagram2021/fiahi/mixin/ItemStackMixin.java
@@ -1,8 +1,10 @@
 package com.hexagram2021.fiahi.mixin;
 
+import com.hexagram2021.fiahi.common.util.FIAHILogger;
 import com.hexagram2021.fiahi.register.FIAHICapabilities;
 import dev.momostudios.coldsweat.api.util.Temperature;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,6 +17,9 @@ public class ItemStackMixin {
 	@Inject(method = "inventoryTick", at = @At(value = "TAIL"))
 	public void tickFood(Level level, Entity entity, int slot, boolean selected, CallbackInfo ci) {
 		ItemStack current = (ItemStack)(Object)this;
-		current.getCapability(FIAHICapabilities.FOOD_CAPABILITY).ifPresent(c -> c.foodTick(Temperature.getTemperatureAt(entity.blockPosition(), entity.level)));
+		if(entity instanceof LivingEntity livingEntity) {
+			double temp = Temperature.get(livingEntity, Temperature.Type.CORE);
+			current.getCapability(FIAHICapabilities.FOOD_CAPABILITY).ifPresent(c -> c.foodTick(temp));
+		}
 	}
 }
