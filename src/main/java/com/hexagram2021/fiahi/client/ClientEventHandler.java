013a5fa9676c878c087d8c62f2a0dfe127fe18d0
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-08-31 14:24:00 +0800

    优化食物Tooltips，为食物口袋加入tooltip

--- src/main/java/com/hexagram2021/fiahi/client/ClientEventHandler.java
+++ src/main/java/com/hexagram2021/fiahi/client/ClientEventHandler.java
@@ -1,8 +1,13 @@
 package com.hexagram2021.fiahi.client;
 
+import com.hexagram2021.fiahi.common.item.FoodPouchItem;
 import com.hexagram2021.fiahi.register.FIAHICapabilities;
+import com.hexagram2021.fiahi.register.FIAHIItems;
 import com.hexagram2021.fiahi.register.FIAHIMobEffects;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.util.Mth;
@@ -24,18 +29,24 @@ public class ClientEventHandler {
 		ItemStack itemStack = event.getItemStack();
 		if(itemStack.isEdible()) {
 			itemStack.getCapability(FIAHICapabilities.FOOD_CAPABILITY).ifPresent(c -> {
-				Component status = new TranslatableComponent("item.fiahi.temperature.normal");
+				Component status = new TranslatableComponent("item.fiahi.temperature.normal").withStyle(ChatFormatting.GRAY);
 				if(c.getFrozenLevel() > 0) {
-					status = new TranslatableComponent("item.fiahi.temperature.frozen.%d".formatted(Mth.clamp(c.getFrozenLevel(), 0, 3)));
+					status = new TranslatableComponent("item.fiahi.temperature.frozen.%d".formatted(Mth.clamp(c.getFrozenLevel(), 0, 3))).withStyle(ChatFormatting.DARK_AQUA);
 				}
 				if(c.getRottenLevel() > 0) {
-					status = new TranslatableComponent("item.fiahi.temperature.rotten.%d".formatted(Mth.clamp(c.getRottenLevel(), 0, 3)));
+					status = new TranslatableComponent("item.fiahi.temperature.rotten.%d".formatted(Mth.clamp(c.getRottenLevel(), 0, 3))).withStyle(ChatFormatting.DARK_RED);
 				}
 				event.getToolTip().add(status);
 				if(Minecraft.getInstance().options.advancedItemTooltips) {
 					event.getToolTip().add(new TranslatableComponent("item.fiahi.temperature.description", (int)c.getTemperature()));
 				}
 			});
+		} else if(itemStack.is(FIAHIItems.FOOD_POUCH.get()) && Minecraft.getInstance().options.advancedItemTooltips) {
+			CompoundTag nbt = itemStack.getTag();
+			if(nbt != null && nbt.contains("temperature", Tag.TAG_ANY_NUMERIC)) {
+				event.getToolTip().add(new TranslatableComponent("gui.fiahi.count.description", FoodPouchItem.getItemCount(nbt)));
+				event.getToolTip().add(new TranslatableComponent("item.fiahi.temperature.description", (int)nbt.getDouble("temperature")));
+			}
 		}
 	}
 
