8e844cb318fb34baff7556a60ca4a3b30f4750af
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-09-16 18:27:26 +0800

    匹配腐烂的食物，将残渣食用后的效果扩充到两个

--- src/main/java/com/hexagram2021/fiahi/register/FIAHIFoods.java
+++ src/main/java/com/hexagram2021/fiahi/register/FIAHIFoods.java
@@ -7,10 +7,12 @@ import net.minecraft.world.food.FoodProperties;
 public class FIAHIFoods {
 	public static final FoodProperties LEFTOVER_MEAT = new FoodProperties.Builder()
 			.nutrition(1).saturationMod(0.3F)
-			.effect(() -> new MobEffectInstance(MobEffects.HUNGER, 200, 0), 0.8F)
+			.effect(() -> new MobEffectInstance(MobEffects.HUNGER, 400, 0), 0.8F)
+			.effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 400, 0), 0.8F)
 			.meat().build();
 	public static final FoodProperties LEFTOVER_VEGETABLE = new FoodProperties.Builder()
 			.nutrition(1).saturationMod(0.1F)
-			.effect(() -> new MobEffectInstance(MobEffects.HUNGER, 200, 0), 0.8F)
+			.effect(() -> new MobEffectInstance(MobEffects.HUNGER, 400, 0), 0.8F)
+			.effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 400, 0), 0.8F)
 			.build();
 }
