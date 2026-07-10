20d92aee5ebecc27d02edeb6d6d2b5bd8f83af95
liudongyu <10185101162@stu.ecnu.edu.cn>
2026-07-10 12:23:45 +0800

    docs: 添加 event 包的 package-info.java

--- src/main/java/com/hexagram2021/fiahi/common/event/package-info.java
+++ src/main/java/com/hexagram2021/fiahi/common/event/package-info.java
@@ -1,8 +1,15 @@
+/**
+ * FIAHI 事件包喵~
+ *
+ * 包含 FIAHI 暴露给附属模组的事件类喵~
+ *
+ * @author liudongyu
+ */
 @FieldsAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
 
-package com.hexagram2021.fiahi.common;
+package com.hexagram2021.fiahi.common.event;
 
 import net.minecraft.FieldsAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
